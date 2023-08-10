64b99ecb18350136c56e657caba23f2a2ac9b719
TaeHyun <54016309+hhh88261@users.noreply.github.com>
2023-08-10 17:30:20 +0900

    Update views.py

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-# Create your views here.
-
+from .models import User
 
 def login_view(request):
     if request.method == "POST":
+        #아이디, 비밀번호 값 얻어옴
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(username=username, password=password)
@@ -17,5 +17,15 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("accountapp:login")
+    
 def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        #데이터베이스에 정보 추가
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return redirect('accountapp:login')
     return render(request, 'signup.html')
